9840ffe85a6f475e87644f2233144a8c72c81882
Zahra <mirzakizahra@gmail.com>
2021-12-14 13:51:09 -0800

    Add deposit method

--- src/Service.java
+++ src/Service.java
@@ -22,7 +22,7 @@ public class Service {
         session.save(account);
         transaction.commit();
         session.close();
-         //  sessionFactory.close();
+        //  sessionFactory.close();
 
     }
 
@@ -42,67 +42,82 @@ public class Service {
     public void close() {
         sessionFactory.close();
     }
-    public List<User> findByFirstName(String firstName){
+
+    public List<User> findByFirstName(String firstName) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
-        String sql="select * from user where firstName= :firstName";
-        SQLQuery query=session.createSQLQuery(sql);
+        String sql = "select * from user where firstName= :firstName";
+        SQLQuery query = session.createSQLQuery(sql);
         query.addEntity(User.class);
-        query.setParameter("firstName",firstName);
-        List<User>list= query.list();
+        query.setParameter("firstName", firstName);
+        List<User> list = query.list();
         session.close();
         return list;
 
     }
-    public List<User> findByLastName(String lastName){
+
+    public List<User> findByLastName(String lastName) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
-        String sql="select * from user where lastName = :lastName";
-        SQLQuery query=session.createSQLQuery(sql);
+        String sql = "select * from user where lastName = :lastName";
+        SQLQuery query = session.createSQLQuery(sql);
         query.addEntity(User.class);
-        query.setParameter("lastName",lastName);
-        List<User>list= query.list();
+        query.setParameter("lastName", lastName);
+        List<User> list = query.list();
         session.close();
         return list;
 
     }
-    public User findByCardNumber(int cardNumber){
+
+    public User findByCardNumber(int cardNumber) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
-        String sql="select * from account where cardNumber = :cardNumber";
-        SQLQuery query=session.createSQLQuery(sql);
+        String sql = "select * from account where cardNumber = :cardNumber";
+        SQLQuery query = session.createSQLQuery(sql);
         query.addEntity(Account.class);
-        query.setParameter("cardNumber",cardNumber);
-        Account account= (Account) query.list().get(0);
+        query.setParameter("cardNumber", cardNumber);
+        Account account = (Account) query.list().get(0);
         session.close();
         return account.getUser();
 
     }
-    public Account findAccountByCardNumber(int cardNumber){
+
+    public Account findAccountByCardNumber(int cardNumber) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
-        String sql="select * from account where cardNumber = :cardNumber";
-        SQLQuery query=session.createSQLQuery(sql);
+        String sql = "select * from account where cardNumber = :cardNumber";
+        SQLQuery query = session.createSQLQuery(sql);
         query.addEntity(Account.class);
-        query.setParameter("cardNumber",cardNumber);
-        Account account= (Account) query.list().get(0);
+        query.setParameter("cardNumber", cardNumber);
+        Account account = (Account) query.list().get(0);
         session.close();
         return account;
 
     }
-    public void withdraw(int caredNumber,int fee){
-        Account account= findAccountByCardNumber(caredNumber);
+
+    public void withdraw(int caredNumber, int fee) {
+        Account account = findAccountByCardNumber(caredNumber);
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
-        account = session.load(Account.class,account.getId());
-        float temp=account.getBalance()-fee;
+        account = session.load(Account.class, account.getId());
+        float temp = account.getBalance() - fee;
         account.setBalance(temp);
         session.update(account);
         transaction.commit();
         session.close();
 
 
-
+    }
+    public void deposit(int cardNumber,int fee){
+        Account account = findAccountByCardNumber(cardNumber);
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        account = session.load(Account.class, account.getId());
+        float temp = account.getBalance() + fee;
+        account.setBalance(temp);
+        session.update(account);
+        transaction.commit();
+        session.close();
     }
 
 
